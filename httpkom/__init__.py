c676b78dee03de064cd4c41c8e7bfda58f7a0134
Oskar Skoog <oskar@osd.se>
2012-05-19 19:06:26 +0200

    Changed to port 5001 for debugging. Implemented some CORS handling.

--- httpkom/__init__.py
+++ httpkom/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from flask import render_template
+from flask import render_template, request
 
 from flaskapp import app
 
@@ -8,6 +8,33 @@ from flaskapp import app
 import conferences
 import sessions
 import texts
+
+
+crossdomain_allowed_origins = [ 'https://jskom.osd.se', 'http://localhost:5000' ]
+crossdomain_allow_headers = [ 'Origin', 'Accept', 'Cookie', 'Content-Type',
+                              'X-Requested-With' ]
+crossdomain_expose_headers = [ 'Set-Cookie', 'Cookie' ]
+crossdomain_max_age = 1 #21600
+crossdomain_allow_methods = [ 'GET', 'POST', 'PUT', 'DELETE', 'OPTIONS' ]
+
+
+@app.after_request
+def allow_crossdomain(resp):
+    if 'Origin' in request.headers:
+        h = resp.headers
+        origin = request.headers['Origin']
+        if (origin in crossdomain_allowed_origins
+            or '*' in crossdomain_allowed_origins):
+            h['Access-Control-Allow-Origin'] = origin
+            h['Access-Control-Allow-Methods'] = ', '.join(crossdomain_allow_methods)
+            h['Access-Control-Allow-Credentials'] = 'true'
+            h['Access-Control-Max-Age'] = str(crossdomain_max_age)
+            h['Access-Control-Allow-Headers'] = ', '.join(crossdomain_allow_headers)
+            h['Access-Control-Expose-Headers'] = ', '.join(crossdomain_expose_headers)
+        else:
+            h['Access-Control-Allow-Origin'] = 'null'
+    
+    return resp
 
 
 @app.route("/")
--- run-httpkom.py
+++ run-httpkom.py
@@ -6,4 +6,4 @@ from httpkom import app
 
 if __name__ == "__main__":
     app.debug = True
-    app.run()
+    app.run(port=5001)
