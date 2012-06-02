19e64168b611fb8ec5e04ce2b7605fe39413fae2
Oskar Skoog <oskar@osd.se>
2012-06-02 11:32:57 +0200

    Testing with the old StringIO module instead, because BytesIO does not have suppor for fileno(), which is required by gunicorns wsgi implementation.

--- httpkom/texts.py
+++ httpkom/texts.py
@@ -2,6 +2,7 @@
 # Copyright (C) 2012 Oskar Skoog. Released under GPL.
 
 import io
+import StringIO
 
 from flask import g, request, jsonify, send_file, Response
 
@@ -120,7 +121,8 @@ def texts_get_body(text_no):
         text = g.ksession.get_text(text_no)
         mime_type, encoding = parse_content_type(text.content_type)
         
-        data = io.BytesIO()
+        #data = io.BytesIO()
+        data = StringIO.String()
         if mime_type[0] == 'text':
             data.write(text.body.encode('utf-8'))
         else:
