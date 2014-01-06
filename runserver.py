3eee4e79c2710a63df79947747598a9d53892391
Oskar Skoog <oskar@osd.se>
2014-01-06 20:34:10 +0100

    Cleanup of old gevent test code.

--- runserver.py
+++ runserver.py
@@ -1,19 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-#from gevent.pywsgi import WSGIServer
-#from gevent import monkey; monkey.patch_all()
-
 from httpkom import app
 
 
 if __name__ == "__main__":
     app.run(port=5001)
-    #address = 'localhost', 5001
-    #http_server = WSGIServer(address, app)
-    #try:
-    #    print "Server running on port %s:%d. Ctrl+C to quit" % address
-    #    http_server.serve_forever()
-    #except KeyboardInterrupt:
-    #    http_server.stop()
-    #    print "Stopped."
