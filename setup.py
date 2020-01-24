e5f0fcb313af52a2faaeeac464b4e765918024ed
Oskar Skoog <oskar@osd.se>
2020-01-24 09:53:32 +0100

    Update setup.py with some minor things.

--- setup.py
+++ setup.py
@@ -1,14 +1,34 @@
-from setuptools import setup
+import setuptools
+
 from httpkom.version import __version__
 
-setup(
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
     name='httpkom',
     version=__version__,
+    description='REST-like API for LysKOM',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author='Oskar Skoog',
     author_email='oskar@osd.se',
-    long_description=__doc__,
+    url='https://github.com/osks/httpkom',
     packages=['httpkom'],
+    classifiers=[
+        'License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)',
+    ],
     include_package_data=True,
     zip_safe=False,
-    install_requires=['Flask', 'mimeparse', 'Sphinx', 'pylyskom', 'six', 'CherryPy', 'Paste']
+    #python_requires='>=2.7, >=3.7',
+    install_requires=[
+        'Flask',
+        'mimeparse',
+        'Sphinx',
+        'pylyskom',
+        'six',
+        'CherryPy',
+        'Paste'
+    ]
 )
