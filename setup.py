fe867a3eb3a33fc55d6cce745c20f63717df8ed5
Oskar Skoog <oskar@osd.se>
2020-02-07 12:55:25 +0100

    Add +dev suffix to version.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.rst", "r") as fh:
 
 setuptools.setup(
     name='httpkom',
-    version='0.14',
+    version='0.14+dev',
     description='REST-like HTTP API for LysKOM',
     long_description=long_description,
     long_description_content_type="text/x-rst",
