92ce18e97f42c1bd6d9dc89735f8d40eadde9ea7
Oskar Skoog <oskar@osd.se>
2020-07-03 11:13:31 +0200

    Fix argparse description

--- httpkom/__main__.py
+++ httpkom/__main__.py
@@ -37,7 +37,7 @@ def run_http_server(args):
 def main():
     logging.basicConfig(format='%(asctime)s %(levelname)-7s %(name)-15s %(message)s', level=logging.DEBUG)
 
-    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser = argparse.ArgumentParser(description='Httpkom')
 
     parser.add_argument('--config', help='Path to configuration file',
                         required=True)
