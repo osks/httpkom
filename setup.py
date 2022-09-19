452685aad4c4051f8acf978b2b5465faede6bd47
Oskar Skoog <oskar@osd.se>
2022-09-19 10:10:23 +0200

    Increase version to 0.21+dev

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.rst", "r") as fh:
 
 setuptools.setup(
     name='httpkom',
-    version='0.21',
+    version='0.21+dev',
     description='REST-like HTTP API for LysKOM',
     long_description=long_description,
     long_description_content_type="text/x-rst",
